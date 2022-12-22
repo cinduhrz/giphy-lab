6728a6ea17c8595e193d6c260de5704f3b322376
Cindy Wong <cindywongdev@gmail.com>
2022-12-21 21:48:20 -0500

    set up skeleton for Form and GifDisplay components

---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+function Form(props){
+    return(<>
+        <h1>Form with Button</h1>
+    </>)
+}
+
+export default Form
---
+++ src/components/GifDisplay.js
@@ -0,0 +1,7 @@
+function GifDisplay(props){
+    return(<>
+        <h1>Gif Display</h1>
+    </>)
+}
+
+export default GifDisplay
